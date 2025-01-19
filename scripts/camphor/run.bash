750be289701b6a4a6e83ac09073b7a76d796eb56
RtrMmmt <ryotaro.manmoto@gmail.com>
2025-01-19 15:45:13 +0900

    fix

--- scripts/camphor/run.bash
+++ scripts/camphor/run.bash
@@ -1,13 +1,10 @@
 #!/bin/bash
 #============ SBATCH Directives =======
-#SBATCH --account=b38307             # アカウント名 (ジョブグループ)
-#SBATCH --partition=jha              # パーティション名 (リソースグループ)
-#SBATCH --nodes=4                    # 使用するノード数
-#SBATCH --ntasks=8                   # MPIプロセス数
-#SBATCH --time=00:30:00              # 実行時間
-#SBATCH --output=output.out          # 出力ファイル
-#SBATCH --error=output.out           # エラーログを標準出力に統合
-#SBATCH --rsc p=8:t=56:c=1      　　　# プロセスごとのリソース指定 (必要に応じて変更)
+#SBATCH -p=jha
+#SBATCH -t=00:30:00
+#SBATCH --output=output.out
+#SBATCH --error=output.out
+#SBATCH --rsc p=4:t=8:c=8
 
 #============ Shell Script ============
 
