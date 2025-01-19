3dd62a0b97a717b893b5b5716755e88ae46ffaff
RtrMmmt <ryotaro.manmoto@gmail.com>
2025-01-19 15:16:41 +0900

    fix for camphor

--- scripts/grand chariot/run.bash
+++ scripts/grand chariot/run.bash
@@ -3,8 +3,10 @@
 #PJM -L "rscgrp=ea"
 #PJM -L "node=4"
 #PJM --mpi "proc=8"
-#PJM --omp "thread=20"
 #PJM -L "elapse=30:00"
 #PJM -o "output.out"
 #PJM -j
-mpirun ./solver data/ss.mtx
+
+export OMP_NUM_THREADS=20
+export I_MPI_PIN_DOMAIN=40
+mpiexec.hydra -n ${PJM_MPI_PROC} ./solver data/5_ss.mtx
