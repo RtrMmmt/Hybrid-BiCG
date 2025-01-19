0e23b29ae526e387afa03a7a4b1505a164a89fc7
RtrMmmt <ryotaro.manmoto@gmail.com>
2025-01-19 15:37:01 +0900

    fix

--- run.bash
+++ run.bash
@@ -1,14 +1,15 @@
 #!/bin/bash
-
 #============ SBATCH Directives =======
-#SBATCH -p jha
-#SBATCH -t 0:30:0
-#SBATCH --nodes=4
-#SBATCH --ntasks-per-node=2
-#SBATCH --cpus-per-task=56
-#SBATCH --output=output.out
-#SBATCH --error=output.out
+#SBATCH --account=b38307             # アカウント名 (ジョブグループ)
+#SBATCH --partition=jha              # パーティション名 (リソースグループ)
+#SBATCH --nodes=4                    # 使用するノード数
+#SBATCH --ntasks=8                   # MPIプロセス数
+#SBATCH --time=00:30:00              # 実行時間
+#SBATCH --output=output.out          # 出力ファイル
+#SBATCH --error=output.out           # エラーログを標準出力に統合
+#SBATCH --rsc p=8:t=56:c=1      　　　# プロセスごとのリソース指定 (必要に応じて変更)
 
 #============ Shell Script ============
-export OMP_NUM_THREADS=56
+
+# MPIプログラムの実行
 srun ./solver data/5_ss.mtx
