6d7d003d3e1de5cd47efc9feabe4fcaab62c5c40
RtrMmmt <ryotaro.manmoto@gmail.com>
2025-02-07 08:13:48 +0900

    fix

--- src/openmp_vector.c
+++ src/openmp_vector.c
@@ -5,7 +5,7 @@ void my_openmp_daxpy(int n, double alpha, const double *x, double *y) {
     for (int i = 0; i < n; i++) {
         y[i] += alpha * x[i];
     }
-    //#pragma omp barrier //追加
+    #pragma omp barrier //追加
 }
 
 double my_openmp_ddot(int n, const double *x, const double *y) {
@@ -16,7 +16,7 @@ double my_openmp_ddot(int n, const double *x, const double *y) {
     for (int i = 0; i < n; i++) {
         sum += x[i] * y[i];
     }
-    //#pragma omp barrier //追加
+    #pragma omp barrier //追加
     return sum;
 }
 
@@ -25,7 +25,7 @@ void my_openmp_dscal(int n, double alpha, double *x) {
     for (int i = 0; i < n; i++) {
         x[i] *= alpha;
     }
-    //#pragma omp barrier //追加
+    #pragma omp barrier //追加
 }
 
 void my_openmp_dcopy(int n, const double *x, double *y) {
@@ -33,7 +33,7 @@ void my_openmp_dcopy(int n, const double *x, double *y) {
     for (int i = 0; i < n; i++) {
         y[i] = x[i];
     }
-    //#pragma omp barrier //追加
+    #pragma omp barrier //追加
 }
 
 void my_openmp_ddot_v2(int n, const double *x, const double *y, double *global_dot) {
@@ -61,5 +61,5 @@ void openmp_set_vector_zero(int vec_loc_size, double *vec) {
     for (int l = 0; l < vec_loc_size; l++) {
         vec[l] = 0.0;
     }
-    //#pragma omp barrier //追加
+    #pragma omp barrier //追加
 }
